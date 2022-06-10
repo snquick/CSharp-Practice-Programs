98d58e289e3f2b70009a09c12c60d00bb3b1e7f3
snquick <stephanienquick@lewisu.edu>
2022-06-09 20:30:43 -0500

    finished swapping values program

---
+++ SwappingValues/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwappingValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // initalizing variables
+            int num1, num2, temp;
+            
+            // welcome text
+            Console.WriteLine("***Welcome to swap your numbers corporation! If you need two numbers swapped, you came to the right place***");
+
+            // user input and scanning the values
+            Console.WriteLine("Enter the first number please: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number please: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            // values of the two variables are being swapped with each other without creating new storage location for the variables
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+
+            Console.WriteLine("Here is the swapped value for Number 1: "+ num1);
+            Console.WriteLine("Here is the swapped value for Number 2: " + num2);
+            Console.WriteLine("");
+
+            // Quitting program
+            Console.WriteLine("Please press <q> to exit the program...");
+            while (Console.ReadKey().Key != ConsoleKey.Q) { }
+
+        }
+    }
+}
